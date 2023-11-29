29c3901da91b13761baf9188650902155daba9bc
fringemonkey89 <markomajeticjr@gmail.com>
2023-11-29 12:40:03 -0500

    created home page

--- server/index.js
+++ server/index.js
@@ -2,12 +2,31 @@ const express = require('express');
 const app = express();
 const PORT = 4000;
 
+const http = require('http').Server(app);
+const cors = require('cors');
+
+app.use(cors());
+
+const socketIO = require('socket.io')(http, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+});
+
+
+socketIO.on('connection', (socket) => {
+    console.log(`${socket.id} user just connected`)
+    socket.on('disconnect', () => {
+        console.log('user disconnected')
+    })
+})
+
 app.get('/api', (req, res) => {
   res.json({
     message: 'Hello world',
   });
 });
 
-app.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
