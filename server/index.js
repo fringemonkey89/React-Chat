b5eb778a711aba741c51ac357f1dc3cfed4192d6
fringemonkey89 <markomajeticjr@gmail.com>
2023-12-03 13:18:39 -0500

    added comments to serverside sockets

--- server/index.js
+++ server/index.js
@@ -17,27 +17,43 @@ const socketIO = require('socket.io')(http, {
 
 let users = [];
 
+// this line establishes a connection event listener, triggered when a client connects tp the server
+//the callbac function is executed when the connection is made, with the socket object representing 
+//the connected socket
 socketIO.on('connection', (socket) => {
+  // log a message indicating that a user has just connected along with their socket ID
     console.log(`${socket.id} user just connected`)
 
+    //this line sets up a message event listener for the connected socket
+    // the callback function is executed when a message event is received from the client
+    // the data parameter represents the received message data
     socket.on('message', (data) => {
       console.log(data)
+      //emit a 'messageResponse' event with the updated users array to all connected sockets
+      // the data parameter is sent along with the event
       socketIO.emit('messageResponse', data)
     })
 
+    // this line sets up a new user event listener for the connected socket
+    //the call back function is executed when a newUser event is received from the client
+    //the data parameter represents the received user data
     socket.on('newUser', (data) => {
-      
+      //push the received user data into the users array
       users.push(data)
 
+      //emit a newUserResponse event with the the updated users array to all connected sockets
+      // the users array is sent along with the event, representing the updated list of users
       socketIO.emit('newUserResponse', users)
     })
 
     socket.on('disconnect', () => {
+      // log a message indicating that a user has just disconnected
         console.log('user disconnected')
 
         users = users.filter((user) => user.socketID !== socket.id);
-
+        //emit a newUserResponse event with the updated users array to all connected sockets
         socketIO.emit('newUserResponse', users)
+        //disconnect the socket
         socket.disconnect();
     })
 })
